94ed2723de978193760832976326258a559fb332
Jayson.json <jaysonjson@web.de>
2020-10-10 02:02:28 +0200

    p

--- src/main/java/jayson/json/zapan/events/PlayerJoin.java
+++ src/main/java/jayson/json/zapan/events/PlayerJoin.java
@@ -1,6 +1,7 @@
 package jayson.json.zapan.events;
 
 import jayson.json.zapan.Utility;
+import jayson.json.zapan.inventories.GuildChunkInventory;
 import jayson.json.zapan.npc.NPC;
 import jayson.json.zapan.other.Scoreboard;
 import org.bukkit.event.EventHandler;
@@ -15,6 +16,8 @@ public class PlayerJoin implements Listener {
         Utility.refreshHearts(event.getPlayer());
         Scoreboard.updateScoreboard(event.getPlayer());
         NPC.sendSinglePacket(event.getPlayer());
+        GuildChunkInventory inventory = new GuildChunkInventory();
+        inventory.openInventory(event.getPlayer());
     }
 
 }
---
+++ src/main/java/jayson/json/zapan/inventories/GuildChunkInventory.java
@@ -0,0 +1,53 @@
+package jayson.json.zapan.inventories;
+
+import jayson.json.zapan.Zapan;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import javax.annotation.Nullable;
+
+public class GuildChunkInventory implements Listener {
+
+    @Nullable
+    Inventory inventory;
+
+    public GuildChunkInventory() {
+        Bukkit.getPluginManager().registerEvents(this, Zapan.INSTANCE);
+    }
+
+    public void openInventory(Player player) {
+        inventory = Bukkit.createInventory(player, 54, "Guilden Gebiete");
+        setGlasses();
+        player.openInventory(inventory);
+    }
+
+    private void setGlasses() {
+        for (int i = 0; i < 54; i++) {
+            inventory.setItem(i, new ItemStack(Material.WHITE_STAINED_GLASS_PANE));
+        }
+
+        ItemStack orangePane = new ItemStack(Material.ORANGE_STAINED_GLASS_PANE);
+        orangePane.setAmount(5);
+        ItemMeta orangePaneMeta = orangePane.getItemMeta();
+        orangePaneMeta.setDisplayName("Chunks verbleibend");
+        orangePane.setItemMeta(orangePaneMeta);
+
+        inventory.setItem(2, orangePane);
+        inventory.setItem(6, new ItemStack(Material.PURPLE_STAINED_GLASS_PANE));
+        inventory.setItem(31, new ItemStack(Material.RED_STAINED_GLASS_PANE));
+    }
+
+    @EventHandler
+    public void InventoryClick(InventoryClickEvent event) {
+        if(event.getInventory().equals(inventory)) {
+            event.setCancelled(true);
+        }
+    }
+}
